447b34acce2177025c8a18b7290851112eda49b2
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-04-03 17:26:08 +0200

    Updates

---
+++ precache-manifest.c25f26787e67fd57e7deac996b18b5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "0df4f556856815c0fe7a",
+    "url": "/react-app/static/js/main.b907a418.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0df4f556856815c0fe7a",
+    "url": "/react-app/static/css/main.58ac8d3f.chunk.css"
+  },
+  {
+    "revision": "709d087e1ef16c0ea36cdd77221eb421",
+    "url": "/react-app/index.html"
+  }
+];
